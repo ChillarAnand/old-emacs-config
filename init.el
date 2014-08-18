4bdc20030d6d5e24fccec2f4738211db52157903
K3 <anand.r@leucinerichbio.com>
2014-08-18 16:41:49 +0530

    new configs

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el --- Where all the magic begins
+;; init.el --- Where all the magic begins
 ;;
 ;; Part of the Emacs Starter Kit
 ;;
@@ -25,6 +25,10 @@
 (add-to-list 'load-path dotfiles-dir)
 
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+
+;; for removing the warning at startup 
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
@@ -76,4 +80,109 @@
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 (if (file-exists-p user-specific-config) (load user-specific-config))
 
-;;; init.el ends here
+
+;; for full screen 
+(defun toggle-fullscreen ()
+  (interactive)
+    (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                                         '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+        (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                                         '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+)
+(toggle-fullscreen)
+
+;; use Shift+arrow_keys to move cursor around split panes
+(windmove-default-keybindings)
+;; when cursor is on edge, move to the other side, as in a toroidal space
+(setq windmove-wrap-around t )
+
+;; jedi - auto complete python
+;;(add-hook 'python-mode-hook 'jedi:setup)
+;;(setq jedi:complete-on-dot t)                 ; optional
+
+;; all packages archives
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; emacs-for-python
+(load-file "~/.emacs.d/emacs-for-python/epy-init.el")
+
+;; for tab indent
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+;; stop auto indent
+(setq indent-tabs-mode nil)
+
+
+;; -*- mode: elisp -*-
+;; Disable splash screen(to enable it again replace t with 0)
+(setq inhibit-splash-screen t)
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;;;;org mode-configuration
+;; Enable org mode
+(require 'org)
+;; Make org-mode work with files ending in org.
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; The above is default in emacsen
+
+
+;; ;; web browsing
+;; ;;change default browser for 'browse-url'  to w3m
+;; (setq browse-url-browser-function 'w3m-goto-url-new-session)
+ 
+;; ;;change w3m user-agent to android
+;; (setq w3m-user-agent "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.")
+ 
+;; ;;quick access hacker news
+;; (defun hn ()
+;;   (interactive)
+;;   (browse-url "http://news.ycombinator.com"))
+
+;; save desktop
+(desktop-save-mode 1)
+
+
+;; OPEN TERMINAL
+(global-set-key [f9] 'ansi-term)
+
+
+;; real auto save 
+(require 'real-auto-save)
+(add-hook 'text-mode-hook 'turn-on-real-auto-save)
+(add-hook 'muse-mode-hook 'turn-on-real-auto-save)
+
+(setq real-auto-save-interval 5) ;; in seconds
+
+(turn-on-real-auto-save)
+
+
+
+;; auto save buffers - while switching buffers
+;; use shift + arrow keys to switch between visible buffers
+(require 'windmove)
+(windmove-default-keybindings 'super)
+
+;; automatically save buffers associated with files on buffer switch
+;; and on windows switch
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+
+
+
+;; init.el ends here
+
